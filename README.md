87cc4e9c49f017adc2c909043d016cf67491deba
Blugon <71944969+Blugon0921@users.noreply.github.com>
2021-09-30 12:16:13 +0900

    Mention

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mention(멘션)
+
+<img src="https://img.shields.io/badge/java-16-FF7700.svg?logo=java" alt="java"/>
+<img src="https://img.shields.io/badge/kotlin-1.5.30-186FCC.svg?logo=kotlin" alt="kotlin"/>
+<img src="https://img.shields.io/badge/papermc-1.17-222222.svg" alt="papermc"/>
+
+@닉네임 <br>
+혹은 <br>
+@ 닉네임 <br>
+로 플레이어 멘션 가능
---
+++ src/main/kotlin/io/github/blugon0921/mention/Mention.kt
@@ -0,0 +1,39 @@
+package io.github.blugon0921.mention
+
+import net.kyori.adventure.text.Component
+import net.md_5.bungee.api.ChatColor
+import org.bukkit.Bukkit
+import org.bukkit.Sound
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerChatEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class Mention : JavaPlugin(),Listener {
+
+    override fun onEnable() {
+        logger.info("Plugin Enable")
+        Bukkit.getPluginManager().registerEvents(this, this)
+    }
+
+    override fun onDisable() {
+        logger.info("Plugin Disable")
+    }
+
+    @EventHandler
+    fun mention(event : PlayerChatEvent) {
+        val player = event.player
+        val message = event.message
+
+        if(message[0] == '@') {
+            if(Bukkit.getPlayer(message.replace("@", "").replace(" ", "")) != null) {
+                val mPlayer = Bukkit.getPlayer(message.replace("@", "").replace(" ", ""))!!
+                mPlayer.playSound(mPlayer.location, Sound.ENTITY_ARROW_HIT_PLAYER, 1f, 0.5f)
+                event.message = "${ChatColor.BLUE}${event.message.replace(" ", "")}"
+                mPlayer.sendActionBar(Component.text("${ChatColor.DARK_GREEN}${player.name}님이 당신을 멘션했습니다!"))
+            } else {
+                player.sendActionBar(Component.text("${ChatColor.RED}알 수 없는 플레이어 입니다"))
+            }
+        }
+    }
+}
